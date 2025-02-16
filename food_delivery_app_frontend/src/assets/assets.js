ec8da6fc0394f533d999a50070bb024b4fa5bd31
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2025-02-16 22:24:02 +0530

    footer complete

--- food_delivery_app_frontend/src/assets/assets.js
+++ food_delivery_app_frontend/src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from './logo.png'
+import logo2 from './logo2.png'
 import bascket from './bascket.png'
 import search from './search.png'
 import header3 from './header3.png'
@@ -50,11 +51,11 @@ import food_32 from './food_32.png'
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
 import remove_icon_red from './remove_icon_red.png'
-// import app_store from './app_store.png'
-// import play_store from './play_store.png'
-// import linkedin_icon from './linkedin_icon.png'
-// import facebook_icon from './facebook_icon.png'
-// import twitter_icon from './twitter_icon.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
 // import cross_icon from './cross_icon.png'
 // import selector_icon from './selector_icon.png'
 import rating_starts from './rating_starts.png'
@@ -65,6 +66,7 @@ import rating_starts from './rating_starts.png'
 
 export const assets ={
     logo,
+    logo2,
     bascket,
     search,
     header3,
@@ -74,11 +76,11 @@ export const assets ={
     add_icon_green,
     add_icon_white,
     remove_icon_red,
-    // app_store,
-    // play_store,
-    // linkedin_icon,
-    // facebook_icon,
-    // twitter_icon,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
     // cross_icon,
     // selector_icon,
     // profile_icon,
